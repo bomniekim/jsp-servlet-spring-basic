3638fc5573006368a952c357a92212588bfcac33
Bomin Kim <bomnie.kim@gmail.com>
2020-05-22 19:35:26 +0900

    jdbc practice

---
+++ jdbc_study/src/main/java/bomnie/jdbc_study/App.java
@@ -0,0 +1,13 @@
+package bomnie.jdbc_study;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
