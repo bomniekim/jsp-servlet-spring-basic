be0d7ee327a56426997e1b79a34618daa3d6054e
Bomin Kim <bomnie.kim@gmail.com>
2020-06-12 22:48:55 +0900

    DispatcherServlet이 실행될 때 읽어들일 자바 config 설정파일

---
+++ guestbook/src/main/java/bomnie/guestbook/config/WebMvcContextConfiguration.java
@@ -0,0 +1,48 @@
+package bomnie.guestbook.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = { "bomnie.guestbook.controller" })
+public class WebMvcContextConfiguration extends WebMvcConfigurerAdapter{
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/css/**").addResourceLocations("/css/").setCachePeriod(31556926);
+        registry.addResourceHandler("/img/**").addResourceLocations("/img/").setCachePeriod(31556926);
+        registry.addResourceHandler("/js/**").addResourceLocations("/js/").setCachePeriod(31556926);
+    }
+ 
+    // default servlet handler를 사용하도록
+    @Override
+    // 매핑 정보가 없는 url정보가 들어왔을 때 DefaultServletHttpReqeustHandler가 처리하도록
+    // WAS의 default servlet이 static한 자원을 읽어서 보여줄 수 있게끔 해주는 설정 포함
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+   
+    @Override
+    // 특정 url에 대한 처리를 컨트롤러 클래스를 작성하지 않고 매핑할 수 있도록
+    public void addViewControllers(final ViewControllerRegistry registry) {
+    		System.out.println("addViewControllers가 호출됩니다. ");
+        registry.addViewController("/").setViewName("index");
+    }
+    
+    @Bean
+    public InternalResourceViewResolver getInternalResourceViewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/views/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+}
+ 
