c969ab87c87a8f759d8ff756a33ea59b9702625b
Bomin Kim <bomnie.kim@gmail.com>
2020-06-13 21:46:22 +0900

    request parameter로 전달될 변수들을 객체화하여 값의 전달이 쉽도록

--- spring_mvc/src/main/java/bomnie/spring_mvc/dto/User.java
+++ spring_mvc/src/main/java/bomnie/spring_mvc/dto/User.java
@@ -1,10 +1,12 @@
 package bomnie.spring_mvc.dto;
 
+// request parameter로 전달될 변수들을 객체화시켜 값의 전달과 이동이 쉽도록!
 public class User {
 	private String name;
 	private int age;
 	private String email;
 	
+	// private한 변수들에 접근하기 위해 getter and setter 설정
 	public String getName() {
 		return name;
 	}
