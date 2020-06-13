299b25bd663322699a49e6002d3e98c10550d75e
Bomin Kim <bomnie.kim@gmail.com>
2020-06-13 21:40:30 +0900

    @RequestParam으로 전달받은 값을 설정된 변수에 저장/ MoodelMap 객체 이용

--- spring_mvc/src/main/java/bomnie/spring_mvc/controller/PlusController.java
+++ spring_mvc/src/main/java/bomnie/spring_mvc/controller/PlusController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class PlusController {
 	@GetMapping(path="/plusform")
 	public String plusform() {
-		return "plusForm";
+		return "plusForm"; // plusform.jsp를 보여줌
 	}
 
 	@PostMapping(path = "/plus")
@@ -20,13 +20,14 @@ public class PlusController {
 		int result = value1 + value2;
 		
 		// request.setAttribute() 를 이용해서 request scope 에 값을 저장할 수도 있지만
-		// request 에 종속되지 않기 위해 스프링이 제공하는 ModelMap 을 이용
+		// request 에 종속되지 않기 위해 스프링이 제공하는 ModelMap 객체를 이용
 		// ModelMap 객체에 값을 추가하면, 스프링이 자동으로 request scope에 매핑시켜줌
+		
 		modelMap.addAttribute("value1", value1);
 		modelMap.addAttribute("value2", value2);
 		modelMap.addAttribute("result", result);
 				
-		return "plusResult";
+		return "plusResult"; // plusResult.jsp를 보여줌
 	}
 	
 }
