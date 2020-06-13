a2cdb4f183e138ccc5b1be5f3e4da2f8fa852977
Bomin Kim <bomnie.kim@gmail.com>
2020-06-13 22:24:17 +0900

    @RequestParam으로 전달받은 값을 설정된 변수에 저장/  ModelMap 객체 이용

--- spring_mvc/src/main/java/bomnie/spring_mvc/controller/PlusController.java
+++ spring_mvc/src/main/java/bomnie/spring_mvc/controller/PlusController.java
@@ -21,7 +21,7 @@ public class PlusController {
 		
 		// request.setAttribute() 를 이용해서 request scope 에 값을 저장할 수도 있지만
 		// request 에 종속되지 않기 위해 스프링이 제공하는 ModelMap 객체를 이용
-		// ModelMap 객체에 값을 추가하면, 스프링이 자동으로 request scope에 매핑시켜줌
+		// ModelMap 객체에 값을 추가하면, 스프링이 자동으로 request scope에 매핑시켜줌.
 		
 		modelMap.addAttribute("value1", value1);
 		modelMap.addAttribute("value2", value2);
