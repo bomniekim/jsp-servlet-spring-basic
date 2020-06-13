fa716c68f1451e98e96d6df563a3b45237aeaf75
Bomin Kim <bomnie.kim@gmail.com>
2020-06-13 22:35:05 +0900

    @PathVariable @RequestHeader

--- spring_mvc/src/main/java/bomnie/spring_mvc/controller/GoodsController.java
+++ spring_mvc/src/main/java/bomnie/spring_mvc/controller/GoodsController.java
@@ -11,24 +11,23 @@ import org.springframework.web.bind.annotation.RequestHeader;
 @Controller
 public class GoodsController {
 
-	// goods/ 뒤의 값을 PathVariable 로 받겠다.
+	// goods/ 뒤의 값을 PathVariable 로 받음
 		@GetMapping(path = "/goods/{id}")
 		public String getGoodsById(@PathVariable(name = "id") int id,
 				@RequestHeader(value = "User-Agent", defaultValue = "myBrowser") String userAgent,
 				HttpServletRequest request,
 				ModelMap model) {
 			
-			// @PathVariable : url 에 포함된 id 값을 int id 변수에 저장
-			// @RequestHeader : request 헤더의 정보를 String userAgent 변수에 저장
+			// @PathVariable : url 에 포함된 id 값을 변수 int id에 저장
+			// @RequestHeader : request 헤더의 정보를 변수 String userAgent에 저장
 			
 			String path = request.getServletPath();
 			
-			// 콘솔 출력
 			System.out.println("id : " + id);
 			System.out.println("user_agent : " + userAgent);
 			System.out.println("path : " + path);
 					
-			// ModelMap 객체에 값 저장
+			// ModelMap 객체에 값 추가
 			model.addAttribute("id", id);
 			model.addAttribute("userAgent", userAgent);
 			model.addAttribute("path", path);
