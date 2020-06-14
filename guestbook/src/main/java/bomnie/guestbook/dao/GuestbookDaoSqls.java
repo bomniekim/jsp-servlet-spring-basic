0c1e999b294a62332bd98df74bbdc33836c21e40
Bomin Kim <bomnie.kim@gmail.com>
2020-06-14 14:47:51 +0900

    jdbcTemplate의 파라미터로 사용할 CRUD 쿼리문 클래스

---
+++ guestbook/src/main/java/bomnie/guestbook/dao/GuestbookDaoSqls.java
@@ -0,0 +1,8 @@
+package bomnie.guestbook.dao;
+
+public class GuestbookDaoSqls {
+	public static final String SELECT_PAGING = "SELECT id, name, content, reg_date FROM guestbook ORDER BY id DESC LIMIT :start, :limit";
+	public static final String DELETE_BY_ID = "DELETE FROM guestbook WHERE id = :id";
+	public static final String SELECT_COUNT = "SELECT count(*) FROM guestbook";
+
+}
