1f09b33a9e96936c3fd42adffb02225a71274354
Bomin Kim <bomnie.kim@gmail.com>
2020-06-14 15:50:18 +0900

    jdbcTemplate의 파라미터로 사용할 쿼리문 설정 클래스

--- guestbook/src/main/java/bomnie/guestbook/dao/GuestbookDaoSqls.java
+++ guestbook/src/main/java/bomnie/guestbook/dao/GuestbookDaoSqls.java
@@ -4,5 +4,6 @@ public class GuestbookDaoSqls {
 	public static final String SELECT_PAGING = "SELECT id, name, content, reg_date FROM guestbook ORDER BY id DESC LIMIT :start, :limit";
 	public static final String DELETE_BY_ID = "DELETE FROM guestbook WHERE id = :id";
 	public static final String SELECT_COUNT = "SELECT count(*) FROM guestbook";
+	// 전체 행 개수 가져오기
 
 }
