27d7d1b30c5c242e2ec3e3f7f35926e4e392c0f2
Bomin Kim <bomnie.kim@gmail.com>
2020-06-18 22:59:20 +0900

    FileInput/OutputStream을 이용한 파일 업로드/다운로드 구현

---
+++ guestbook/src/main/java/bomnie/guestbook/controller/FileController.java
@@ -0,0 +1,76 @@
+package bomnie.guestbook.controller;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+@Controller
+public class FileController {
+
+	@GetMapping("/uploadform")
+	public String uploadform() {
+		return "uploadForm";
+	}
+	
+	@PostMapping("/upload")
+	public String upload(@RequestParam("file") MultipartFile file) {
+		
+		System.out.println("파일 이름 : " + file.getOriginalFilename());
+		System.out.println("파일 크기 : " + file.getSize());
+		
+        try(
+                FileOutputStream fos = new FileOutputStream("/tmp/" + file.getOriginalFilename());
+                InputStream is = file.getInputStream();
+        ){
+        	    int readCount = 0;
+        	    byte[] buffer = new byte[1024];
+            while((readCount = is.read(buffer)) != -1){
+                fos.write(buffer,0,readCount);
+            }
+        }catch(Exception ex){
+            throw new RuntimeException("file Save Error");
+        }
+		
+		
+		return "uploadOK";
+	}
+	
+	@GetMapping("/download")
+	public void download(HttpServletResponse response) {
+
+        // 직접 파일 정보를 변수에 저장해 놨지만, 이 부분이 db에서 읽어왔다고 가정
+		String fileName = "mimo_heart.png";
+		String saveFileName = "/tmp/mimo_heart.png"; 
+		String contentType = "image/png";
+		int fileLength = 115004;
+		
+        response.setHeader("Content-Disposition", "attachment; filename=\"" + fileName + "\";");
+        response.setHeader("Content-Transfer-Encoding", "binary");
+        response.setHeader("Content-Type", contentType);
+        response.setHeader("Content-Length", "" + fileLength);
+        response.setHeader("Pragma", "no-cache;"); // 브라우저가 캐시를 읽지 못하도록 
+        response.setHeader("Expires", "-1;");
+        
+        try(
+                FileInputStream fis = new FileInputStream(saveFileName);
+                OutputStream out = response.getOutputStream();
+        ){
+        	    int readCount = 0;
+        	    byte[] buffer = new byte[1024];
+            while((readCount = fis.read(buffer)) != -1){
+            		out.write(buffer,0,readCount);
+            }
+        }catch(Exception ex){
+            throw new RuntimeException("file Save Error");
+        }
+	}
+}
